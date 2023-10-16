dfbe5989bb3e0069134d090e635d77c22f50e56c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-10-16 12:59:38 +0200

    opt-depend on `default`

--- init.lua
+++ init.lua
@@ -38,6 +38,9 @@ minetest.register_craft({
 	}
 })
 
+local sounds = minetest.get_modpath("default") and default.node_sound_glass_defaults()
+local on_dig = minetest.get_modpath("unifieddyes") and unifieddyes.on_dig
+
 minetest.register_node("framedglass:wooden_framed_glass", {
 	description = "Wooden-framed Glass",
 	drawtype = "glasslike_framed",
@@ -45,7 +48,7 @@ minetest.register_node("framedglass:wooden_framed_glass", {
 	paramtype = "light",
 	sunlight_propagates = true,
 	groups = {cracky=3,oddly_breakable_by_hand=3},
-	sounds = default.node_sound_glass_defaults(),
+	sounds = sounds,
 })
 
 minetest.register_node("framedglass:steel_framed_glass", {
@@ -55,7 +58,7 @@ minetest.register_node("framedglass:steel_framed_glass", {
 	paramtype = "light",
 	sunlight_propagates = true,
 	groups = {cracky=3,oddly_breakable_by_hand=3},
-	sounds = default.node_sound_glass_defaults(),
+	sounds = sounds,
 })
 
 minetest.register_node("framedglass:wooden_framed_obsidian_glass", {
@@ -65,7 +68,7 @@ minetest.register_node("framedglass:wooden_framed_obsidian_glass", {
 	paramtype = "light",
 	sunlight_propagates = true,
 	groups = {cracky=3},
-	sounds = default.node_sound_glass_defaults(),
+	sounds = sounds,
 })
 
 minetest.register_node("framedglass:steel_framed_obsidian_glass", {
@@ -77,8 +80,8 @@ minetest.register_node("framedglass:steel_framed_obsidian_glass", {
 	palette = "unifieddyes_palette_extended.png",
 	airbrush_replacement_node = "framedglass:steel_framed_obsidian_glass_tinted",
 	groups = {cracky=3, ud_param2_colorable = 1},
-	sounds = default.node_sound_glass_defaults(),
-	on_dig = unifieddyes.on_dig,
+	sounds = sounds,
+	on_dig = on_dig,
 })
 
 minetest.register_node("framedglass:steel_framed_obsidian_glass_tinted", {
@@ -96,65 +99,67 @@ minetest.register_node("framedglass:steel_framed_obsidian_glass_tinted", {
 	is_ground_content = true,
 	use_texture_alpha = true,
 	groups = {cracky=3, oddly_breakable_by_hand=3, ud_param2_colorable = 1, not_in_creative_inventory = 1},
-	sounds = default.node_sound_glass_defaults(),
-	on_dig = unifieddyes.on_dig,
+	sounds = sounds,
+	on_dig = on_dig,
 })
 
 -- crafts!
 
-unifieddyes.register_color_craft({
-	output = "framedglass:steel_framed_obsidian_glass_tinted",
-	type = "shapeless",
-	palette = "extended",
-	neutral_node = "framedglass:steel_framed_obsidian_glass",
-	recipe = {
-		"NEUTRAL_NODE",
-		"MAIN_DYE"
+if minetest.get_modpath("unifieddyes") then
+	unifieddyes.register_color_craft({
+		output = "framedglass:steel_framed_obsidian_glass_tinted",
+		type = "shapeless",
+		palette = "extended",
+		neutral_node = "framedglass:steel_framed_obsidian_glass",
+		recipe = {
+			"NEUTRAL_NODE",
+			"MAIN_DYE"
+		}
+	})
+
+	-- Convert old nodes
+
+	local static_colors = {
+		red         =  4*24     ,
+		orange      =  4*24 +  2,
+		yellow      =  4*24 +  4,
+		green       =  4*24 +  8,
+		cyan        =  4*24 + 12,
+		blue        =  4*24 + 16,
+		violet      =  4*24 + 18,
+		magenta     =  4*24 + 20,
+		darkgreen   =  8*24 +  8,
+		pink        =         23,
+		brown       =  8*24 +  2,
+		white       = 10*24     ,
+		grey        = 10*24 +  7,
+		darkgrey    = 10*24 + 11,
+		black       = 10*24 + 15
 	}
-})
 
--- Convert old nodes
-
-local static_colors = {
-	red         =  4*24     ,
-	orange      =  4*24 +  2,
-	yellow      =  4*24 +  4,
-	green       =  4*24 +  8,
-	cyan        =  4*24 + 12,
-	blue        =  4*24 + 16,
-	violet      =  4*24 + 18,
-	magenta     =  4*24 + 20,
-	darkgreen   =  8*24 +  8,
-	pink        =         23,
-	brown       =  8*24 +  2,
-	white       = 10*24     ,
-	grey        = 10*24 +  7,
-	darkgrey    = 10*24 + 11,
-	black       = 10*24 + 15
-}
-
-local old_nodes = {}
-for k, v in pairs(static_colors) do
-	table.insert(old_nodes, "framedglass:steel_framed_obsidian_glass" .. k)
-end
-
-minetest.register_lbm({
-	label = "Convert old framedglass static-colored nodes",
-	name = "framedglass:convert_static",
-	run_at_every_load = false,
-	nodenames = old_nodes,
-	action = function(pos, node)
-		local oldcolor = string.sub(node.name, 40)
-		if oldcolor then
-			minetest.swap_node(pos, {
-				name = "framedglass:steel_framed_obsidian_glass_tinted",
-				param2 = static_colors[oldcolor]
-			})
-		else
-			minetest.swap_node(pos, {
-				name = "framedglass:steel_framed_obsidian_glass",
-				param2 = 0
-			})
-		end
+	local old_nodes = {}
+	for k, v in pairs(static_colors) do
+		table.insert(old_nodes, "framedglass:steel_framed_obsidian_glass" .. k)
 	end
-})
+
+	minetest.register_lbm({
+		label = "Convert old framedglass static-colored nodes",
+		name = "framedglass:convert_static",
+		run_at_every_load = false,
+		nodenames = old_nodes,
+		action = function(pos, node)
+			local oldcolor = string.sub(node.name, 40)
+			if oldcolor then
+				minetest.swap_node(pos, {
+					name = "framedglass:steel_framed_obsidian_glass_tinted",
+					param2 = static_colors[oldcolor]
+				})
+			else
+				minetest.swap_node(pos, {
+					name = "framedglass:steel_framed_obsidian_glass",
+					param2 = 0
+				})
+			end
+		end
+	})
+end
